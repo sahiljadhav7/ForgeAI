97466a7669319dc8d333241f92ccfebf691f42a1
sahiljadhav7 <jadhavsahilcodes@gmail.com>
2026-06-11 23:54:51 +0530

    Added components

---
+++ components/Header.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { ArrowRight, Zap } from "lucide-react";
+import { Show, SignUpButton, UserButton, SignInButton } from "@clerk/nextjs";
+import { Button } from "./ui/button";
+
+const Header = () => {
+  return (
+    <header className="w-full fixedtop-0 left-0 z-50 h-16 border-b border-white/6 bg-white/7 backdrop-blur-md">
+      <nav className="mx-auto flex h-full max-w-7xl items-center justify-between px-4 sm:px-6">
+        <Link href="/">
+          <Image
+            src={"/logo.png"}
+            alt="Logo"
+            width={100}
+            height={100}
+            loading="eager"
+            className="h-9 w-auto rounded-md"
+          />
+        </Link>
+
+        <div className="flex items-center gap-4">
+          <Show when="signed-in">
+            <Link
+              href={"/projects"}
+              className="text-13px font-medium text-white/40 transition-colors hover:text-whtite/80"
+            >
+              Projects
+            </Link>
+            <span className="inline-flex h-8 items-center gap=1.5 rounded-full border border-white/10 bg-white/5 px-3 text-white/70 text-13px font-medium">
+              <Zap className="h-3 w-3 fill-white/70" /> 3 / 40 credits
+            </span>
+            <UserButton />
+          </Show>
+
+          <Show when="signed-out">
+            <SignInButton mode="modal">
+              <Button variant="ghost" size="sm" className={"*:text-white/40"}>
+                Sign In
+              </Button>
+            </SignInButton>
+            <SignUpButton mode="modal">
+              <Button
+                type="button"
+                className={
+                  "h-8 rounded-full font-semibold active:scale-95 px-4 pt-0.5"
+                }
+              >
+                Get started
+                <ArrowRight className=" h-3 w-3 opacity-60" />
+              </Button>
+            </SignUpButton>
+          </Show>
+        </div>
+      </nav>
+    </header>
+  );
+};
+export default Header;
