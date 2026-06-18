5c365e5a9dbf5517cf0e617e98c7231bbea067ee
sahiljadhav7 <jadhavsahil36751@gmail.com>
2026-06-18 12:49:35 +0530

    Added rerouting

---
+++ app/auth/layout.tsx
@@ -0,0 +1,9 @@
+const AuthLayout = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  return <div className="flex justify-center pt-40">{children}</div>;
+};
+
+export default AuthLayout;
